4104049e4dcbbc2414effd900116d77fb93eff47
JiLingXiaoBai <uiojkl3588@qq.com>
2025-01-21 21:36:02 +0800

    load model

--- DrawingATriangle/HelloTriangleApplication.cpp
+++ DrawingATriangle/HelloTriangleApplication.cpp
@@ -29,8 +29,8 @@
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 const int MAX_FRAMES_IN_FLIGHT = 2;
-const std::string MODEL_PATH = "DrawingATriangle/viking_room.obj";
-const std::string TEXTURE_PATH = "DrawingATriangle/viking_room.png";
+const std::string MODEL_PATH = "DrawingATriangle/viking_room_model.obj";
+const std::string TEXTURE_PATH = "DrawingATriangle/viking_room_texture.png";
 
 const std::vector<const char*> validationLayers = {
     "VK_LAYER_KHRONOS_validation"
